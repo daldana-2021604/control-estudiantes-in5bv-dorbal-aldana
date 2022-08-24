8cf9abc634428bcc5c57ac9866eacc401126d02a
daldana-2021604 <106627921+daldana-2021604@users.noreply.github.com>
2022-08-23 18:03:29 -0600

    Add files via upload

---
+++ target/control-estudiantes-in5bv-dorbal-aldana-1.0-SNAPSHOT/assets/js/script.js
@@ -0,0 +1,36 @@
+// Can also be used with $(document).ready()
+$(window).load(function() {
+    $('.flexslider').flexslider({
+      animation: "slide"
+    });
+  });
+
+  // Can also be used with $(document).ready()
+  $(window).load(function(){
+    // Wistia handler.
+    wistiaEmbed = document.getElementById( 'player_1' ).wistiaApi;
+   
+    // Call fitVid before FlexSlider initializes, so the proper initial height can be retrieved.
+    $( '.flexslider' )
+      .fitVids()
+      .flexslider({
+        animation: 'slide',
+        useCSS: false,
+        animationLoop: false,
+        smoothHeight: true,
+        start: function( slider ) {
+          $('body').removeClass( 'loading' );
+        },
+        before: function ( slider ) {
+          wistiaEmbed.pause();
+        }
+    });
+   
+    wistiaEmbed.bind( 'play', function() {
+      jQuery( '.flexslider' ).flexslider( 'pause' );
+    });
+   
+    wistiaEmbed.bind( 'end', function() {
+      jQuery( '.flexslider' ).flexslider( 'play' );
+    });
+  });
